7b519f77e830df235a1fc23732cbd8ef821d9233
anthonyzutter <60969070+anthonyzutter@users.noreply.github.com>
2020-07-03 13:53:59 -0300

    Funções verificarCarta e verificarManilha implementada
    Agora o programa as vezes não imprime as manilhas porem funciona normalmente

--- truco/testes.py
+++ truco/testes.py
@@ -1,10 +1,10 @@
-from truco import Baralho, Jogador
+from truco import Baralho, Jogador, Carta
 
 baralho = Baralho()
 baralho.embaralhar()
 baralho.definirVira(baralho)
-baralho.definirManilha()
-baralho.definirManilhas(baralho)
+manilha = baralho.definirManilha()
+baralho.definirManilhas(manilha)
 
 
 print("\nCarta que virou: ")
@@ -13,6 +13,7 @@ baralho.printarVira()
 print("\nManilhas: ")
 baralho.printarManilhas()
 
+"""
 anthony = Jogador("Anthony")
 anthony.criarMao(baralho)
 print("\n<< Jogador 1 >>")
@@ -22,19 +23,43 @@ riad = Jogador("Riad")
 riad.criarMao(baralho)
 print("\n<< Jogador 2 >>")
 riad.mostrarMao()
+"""
 
 andy = Jogador("Andy")
 andy.criarMao(baralho)
-print("\n<< Jogador 3 >>")
+print("\n<< Jogador 1 >>")
 andy.mostrarMao()
 
 rudy = Jogador("Rudy")
 rudy.criarMao(baralho)
-print("\n<< Jogador 4 >>")
+print("\n<< Jogador 2 >>")
 rudy.mostrarMao()
 
-a = rudy.jogarCarta(3)
-print("\nCarta jogada: ")
-a.printarCarta()
-print("\nMão atual: ")
-rudy.mostrarMao()
+#Testes
+
+carta_jogador_01 = rudy.jogarCarta(3)
+print(f"\nO jogador {rudy.nome} jogou a carta: ")
+carta_jogador_01.printarCarta()
+
+carta_jogador_02 = andy.jogarCarta(3)
+print(f"\nO jogador {andy.nome} jogou a carta: ")
+carta_jogador_02.printarCarta()
+
+carta1 = Carta(carta_jogador_01.retornarNumero(), carta_jogador_01.retornarNaipe())
+carta2 = Carta(carta_jogador_02.retornarNumero(), carta_jogador_02.retornarNaipe())
+
+print("\nCarta ganhadora: ")
+if carta1.numero == manilha and carta2.numero == manilha:
+    m = carta1.verificarManilha(carta1, carta2)
+    m.printarCarta()
+elif carta1.numero == manilha:
+    carta1.printarCarta()
+elif carta2.numero == manilha:
+    carta2.printarCarta()
+else:
+    c = carta1.verificarCarta(carta1, carta2)
+    if c == "Empate":
+        print("Empate")
+    else:
+        c.printarCarta()
+
--- truco/truco.py
+++ truco/truco.py
@@ -1,12 +1,49 @@
 import random
 
 class Carta():
+
+    CARTAS_VALORES = {
+        "3": 10,
+        "2": 9,
+        "1": 8,
+        "13": 7,
+        "12": 6,
+        "11": 5,
+        "7": 4,
+        "6": 3,
+        "5": 2,
+        "4": 1
+    }  
+
+    NAIPES_VALORES = {
+        "Paus": 4,
+        "Copas": 3,
+        "Espadas": 2,
+        "Moles": 1
+    }
+
     def __init__(self, numero, naipe):
         self.numero = numero
         self.naipe = naipe
+    
+    def verificarCarta(self, carta_jogador_01, carta_jogador_02):
+        if self.CARTAS_VALORES[str(carta_jogador_01.numero)] > self.CARTAS_VALORES[str(carta_jogador_02.numero)]:
+            return carta_jogador_01
+        elif self.CARTAS_VALORES[str(carta_jogador_01.retornarNumero())] < self.CARTAS_VALORES[str(carta_jogador_02.retornarNumero())]:
+            return carta_jogador_02
+        else:
+            return "Empate"
+    
+    def verificarManilha(self, carta_jogador_01, carta_jogador_02):
+        if self.NAIPES_VALORES[carta_jogador_01.naipe] > self.NAIPES_VALORES[carta_jogador_02.naipe]:
+            return carta_jogador_01
+        elif self.NAIPES_VALORES[carta_jogador_01.naipe] < self.NAIPES_VALORES[carta_jogador_02.naipe]:
+            return carta_jogador_02
+        else:
+            raise "Erro"
         
     def printarCarta(self):
-        if self.numero == 1:
+        """if self.numero == 1:
             self.numero = "A"
         elif self.numero == 13:
             self.numero = "K"
@@ -14,6 +51,7 @@ class Carta():
             self.numero = "J"
         elif self.numero == 11:
             self.numero = "Q"
+            """
         print(f"{self.numero} de {self.naipe}")
 
     def retornarNumero(self):
@@ -24,6 +62,7 @@ class Carta():
 
 
 class Baralho():
+
     def __init__(self):
         self.vira = []
         self.manilhas = []
@@ -31,7 +70,7 @@ class Baralho():
         self.criarBaralho()
 
     def criarBaralho(self):
-        for i in ["Paus", "Copas", "Espadas", "Ouros"]:
+        for i in ["Paus", "Copas", "Espadas", "Moles"]:
             for n in range(1, 14):
                 if n < 8 or n > 10:
                     self.cartas.append(Carta(n, i))
@@ -46,9 +85,7 @@ class Baralho():
         for v in self.vira:
             return v.retornarNumero() + 1
 
-    def definirManilhas(self, baralho):
-        #Coloca as cartas na lista manilhas porém não remove elas do baralho
-        manilha = baralho.definirManilha()
+    def definirManilhas(self, manilha):
         for m in self.cartas:
             x = m.retornarNumero()
             if x == manilha:
@@ -71,6 +108,7 @@ class Baralho():
 
 
 class Jogador():
+
     def __init__(self, nome):
         self.nome = nome
         self.mao = []
@@ -85,12 +123,19 @@ class Jogador():
     def mostrarMao(self):
         for carta in self.mao:
             carta.printarCarta()
+    
+    def adicionarPonto(self):
+        pass
 
 
 class Jogo():
+
     def __init__(self):
         self.rodadas = []
     
     def iniciarRodada(self):
+        pass
+
+    def verificarGanhador(self):
         pass
 
